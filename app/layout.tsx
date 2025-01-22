27fd9ac92b62f1c6c1789fde67db8978f57b37cf
Riku <134082872+rigozu9@users.noreply.github.com>
2025-01-22 21:36:51 +0200

    sidebar added time: 44:30

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{
-      variables: { colorPrimary: '#624cf5' }
-    }}>
+    <ClerkProvider>
       <html lang="en">
         <body className={cn("font-IBMPlex-antialiased", IBMPlex.variable)}>
           {children}
