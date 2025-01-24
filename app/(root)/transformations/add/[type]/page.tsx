50d6d31756c9378685dde9dc36ef667a4fc6cc8b
Riku <134082872+rigozu9@users.noreply.github.com>
2025-01-24 13:08:12 +0200

    added first parts of the transformation form

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import Header from "@/components/shared/Header"
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants"
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async ( { params: { type } }:
+SearchParamProps) => {
+  const { userId } = await auth();
+  console.log(userId);
+  const transformation = transformationTypes[type];
+
+  if (!userId) redirect('/sign-in')
+
+  const user = await getUserById(userId);
+  // user_2rznEvjjPNsdG2tq90vl9WWV9Fv
   return (
-    <div>AddTransformationTypePage</div>
+    <>
+      <Header 
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+      <TransformationForm 
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
   )
 }
 
